d31d08e30c464890e0f2dd03ceee22cf50c3ed2c
Joey <joelnl74@gmail.com>
2023-08-26 14:11:17 +0200

    small changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 let grid = [];
 let container = {};
+let selectedColor = "#800080";
+let mouseDown = 0;
 
 window.onload = setup;
 
 function setup()
+{
+    const resetbtn = document.getElementById("reset-btn");
+    const colorbtn = document.getElementById("color");
+
+    colorbtn.value = selectedColor;
+
+    resetbtn.addEventListener("click", onClickResetBtn);
+    colorbtn.addEventListener("input", onColorChangedButton);
+
+    setupMouseEvent();
+    setupGrid();
+}
+
+function setupMouseEvent()
+{
+    document.body.onmousedown = function() { 
+    ++mouseDown;
+    }
+    document.body.onmouseup = function() {
+    --mouseDown;
+    }
+}
+
+function setupGrid()
 {
     container = document.getElementById("container");
     
@@ -17,11 +43,35 @@ function setup()
             container.appendChild(square);
 
             square.addEventListener('mouseenter', drawHover);
+            grid.push(square);
         }
     }
 }
 
 function drawHover(e)
 {
-    e.target.style.backgroundColor = " #800080";
+    if (mouseDown === 0)
+    {
+        return;
+    }
+
+    e.target.style.backgroundColor = selectedColor;
+}
+
+function onClickResetBtn(e)
+{
+    resetGrid();
+}
+
+function onColorChangedButton(e)
+{
+    selectedColor = e.target.value;
+}
+
+function resetGrid()
+{
+    for (var i = 0; i < grid.length; i++)
+    {
+        grid[i].style.backgroundColor = "#FFFFFF"
+    }
 }
